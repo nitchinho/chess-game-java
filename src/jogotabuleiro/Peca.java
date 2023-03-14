4c4aea90e7e3a75de4f5f7211fe8bcb558c4f34b
Nitchinho <nitchinho007@gmail.com>
2023-03-14 12:05:48 -0300

    Criado Tabuleiro e Peça

---
+++ src/jogotabuleiro/Peca.java
@@ -0,0 +1,14 @@
+package jogotabuleiro;
+
+public class Peca {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+	
+}
---
+++ src/jogotabuleiro/Tabuleiro.java
@@ -0,0 +1,35 @@
+package jogotabuleiro;
+
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private Peca[][] pecas;
+	
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	
+	
+	
+	
+	
+}
