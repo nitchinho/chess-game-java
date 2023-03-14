4793ec9fd0947aa0935b5692e051f5dfc182b88c
Nitchinho <nitchinho007@gmail.com>
2023-03-14 16:56:14 -0300

    Exceção no Xadrez e Posição do Xadrez

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -24,9 +24,13 @@ public class PartidaXadrez {
 		return matriz;
 	}
 	
+	private void colocandoNovaPeca(char coluna, int linha, PecaXadrez peca) {
+		tabuleiro.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
+	}
+	
 	private void SetupInicial() {
-		tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.BRANCA), new Posicao(2, 1));
-		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.PRETA), new Posicao(0, 4));
-		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BRANCA), new Posicao(7, 4));
+		colocandoNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCA));
+		colocandoNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETA));
+		colocandoNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCA));
 	}
 }
